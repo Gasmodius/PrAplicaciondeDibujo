30d219e0b6a7d1402e975c76b4fb86d3f21341c4
Gasmodius <ecorock10@gmail.com>
2017-11-20 17:29:50 +0100

    Primera Impresion Visual
    Ya va cogiendo la forma que queremos conunos cuentos objetos predefinidos

--- PrAplicacionDibujo/src/praplicaciondibujo/PanelMain.java
+++ PrAplicacionDibujo/src/praplicaciondibujo/PanelMain.java
@@ -1,11 +1,14 @@
 package praplicaciondibujo;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 
 
@@ -27,7 +30,22 @@ public class PanelMain extends JPanel {
         topMenu.add(file);
         topMenu.add(view);
         topMenu.add(alarm);
-                //REllename de JMenuItem-----------------------------------
+        
+        JMenuItem item1=new JMenuItem("Accion 1");
+        JMenuItem item2=new JMenuItem("Accion 2");
+        JMenuItem item3=new JMenuItem("Accion 3");
+        JMenuItem item4=new JMenuItem("Accion 4");
+        JMenuItem item5=new JMenuItem("Accion 5");
+        JMenuItem item6=new JMenuItem("Accion 6");
+        JMenuItem item7=new JMenuItem("Accion 7");
+        file.add(item1);
+        file.add(item2);
+        view.add(item3);
+        view.add(item4);
+        view.add(item5);
+        view.add(item6);
+        alarm.add(item7);
+                //CAmbiar los nombres de los JMenuItem-------------------------------------------------------------------
         bb= new JButton("Hola");
         bAddAlarm= new JButton("Crear nueva Alarma");
         
@@ -35,7 +53,7 @@ public class PanelMain extends JPanel {
         setLayout(new BorderLayout());
         
         //Panel Principal donde se muestra la informacion
-        JPanel panelCenter=new PanelImage("vineta.PNG");
+        JPanel panelCenter=new JPanel();
         panelCenter.setLayout(new BorderLayout());
         add(panelCenter,BorderLayout.CENTER);
         
@@ -45,14 +63,20 @@ public class PanelMain extends JPanel {
         pCenterMenu.add(new JButton("muajajaja"));
         
         //Ventana centrada donde se visualiza el contenido
-        JPanel pCenterDibu=new JPanel();
+        PanelImage pCenterDibu=new PanelImage("vineta.PNG");
+        panelCenter.setPreferredSize(new Dimension(500, 500));//Esto no deberia ser asi
         
         panelCenter.add(pCenterMenu,BorderLayout.NORTH);
         panelCenter.add(pCenterDibu,BorderLayout.CENTER);
         
         //Panel Segundario donde se muestran las alarmas
         JPanel panelAlarm=new JPanel();
+        panelAlarm.setLayout(new GridLayout(6,0));
         panelAlarm.add(bAddAlarm);
+        panelAlarm.add(new JLabel("No olvides ponerlo bonito"));
+        panelAlarm.add(new JLabel("No olvides aprobarme"));
+        panelAlarm.add(new JLabel("A las 18:00 sale el proximo cap"));
+        panelAlarm.add(new JLabel("Todos estos mensajes son predefinidos y hay que borrarlos"));
         add(panelAlarm,BorderLayout.EAST);
     }
 
